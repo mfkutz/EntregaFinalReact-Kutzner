a8ade2a15dc18a63c4538542781b2bccef49462c
Martin Kutzner <mfkutz@gmail.com>
2023-05-12 12:00:28 -0300

    final work

---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import ItemDetail from './ItemDetail'
+import { useParams } from 'react-router-dom'
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
+import { BeatLoader } from 'react-spinners'
+import '../styles/itemDetailContainer.css'
+
+const ItemDetailContainer = () => {
+    //state to save a product
+    const [product, setProduct] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const { pid } = useParams()
+    //Fetch product from Firebase
+    useEffect(() => {
+        const dbFirestore = getFirestore()
+        const queryDoc = doc(dbFirestore, 'productos', pid)
+        getDoc(queryDoc)
+            .then(resp => setProduct({ id: resp.id, ...resp.data() }))
+            .catch(error => { console.log(error) })
+            .finally(() => setIsLoading(false))
+    }, [pid])
+    return (
+        <>
+            {isLoading ?
+                <div className='container-spin'>
+                    <BeatLoader color="#e4db5f" />
+                </div>
+                :
+                <div className='as-detail'>
+                    <ItemDetail {...product} />
+                </div>
+            }
+        </>
+    )
+}
+export default ItemDetailContainer
