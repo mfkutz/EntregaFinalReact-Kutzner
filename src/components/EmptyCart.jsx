a40d92017ad6a23f0f7a705f19a35c42b7eaf4fd
Martin Kutzner <mfkutz@gmail.com>
2023-05-13 08:45:46 -0300

    img cart route

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import cartEmpty from '/public/emptycart.png'
+import cartEmpty from '../img/emptycart.png'
 import '../styles/emptyCart.css'
 
 const EmptyCart = () => {
